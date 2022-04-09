d12be7177165376c54e75b06de0ee59ec849c8a5
Dave Amies <damies13@gmail.com>
2022-04-09 23:24:48 +1000

    pypi

---
+++ pypi_dev.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#
+cd $(dirname "$0")
+
+currversion=$(git describe --abbrev=0 --tags)
+echo $currversion
+subver=$(date +%s)
+if [[ "$currversion" =~ v[0-9]\. ]]; then
+	verparts=($(echo $currversion | tr "-" "\n"))
+	len=${#verparts[@]}
+	if [ $len -gt 1 ]; then
+		txtversion=${verparts[0]}.$subver-${verparts[1]}
+	else
+		txtversion=${verparts[0]}.$subver
+	fi
+	numver=$(echo $currversion | sed -E 's/v([^-]*)/\1/')
+	echo numver\: $numver
+	version=$numver.$subver
+	echo version\: $version
+
+	# sed -i '' -e "s/version *= *\"[^\"]*\"/version = \"${version}\"/" $(find . -name "*.py")
+	sed -i '' -e "s/version = \"[^\"]*\"/version = \"${numver}\"/" $(find . -name "*.py")
+	sed -i '' -e "s/version=\"[^\"]*\"/version=\"${numver}\"/" $(find . -name "*.py")
+	#	Version Test
+	sed -i '' -e "s/#    Version .*/#    Version ${version}/" $(find . -name "*.py")
+
+	rm -R dist/
+	python3 setup*.py sdist bdist_wheel
+
+	# python3 -m twine upload --repository testpypi dist/*${version}*
+
+fi
+
+cd -
---
+++ pypi_relese.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+#
+cd $(dirname "$0")
+
+currversion=$(git rev-parse --abbrev-ref HEAD)
+echo $currversion
+if [[ "$currversion" =~ v[0-9]\. ]]; then
+	verparts=($(echo $currversion | tr "-" "\n"))
+	len=${#verparts[@]}
+	if [ $len -gt 1 ]; then
+		txtversion=${verparts[0]}-${verparts[1]}
+	else
+		txtversion=${verparts[0]}
+	fi
+	numver=$(echo $currversion | sed -E 's/v([^-]*)/\1/')
+	echo numver\: $numver
+	version=$numver
+	echo version\: $version
+
+	# sed -i '' -e "s/version *= *\"[^\"]*\"/version = \"${version}\"/" $(find . -name "*.py")
+	sed -i '' -e "s/version = \"[^\"]*\"/version = \"${numver}\"/" $(find . -name "*.py")
+	sed -i '' -e "s/version=\"[^\"]*\"/version=\"${numver}\"/" $(find . -name "*.py")
+	#	Version Test
+	sed -i '' -e "s/#    Version .*/#    Version ${version}/" $(find . -name "*.py")
+
+	rm -R dist/
+	python3 setup*.py sdist bdist_wheel
+
+	# python3 -m twine upload dist/*${version}*
+
+fi
+
+cd -
